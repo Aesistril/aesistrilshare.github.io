890746ca763831d89dc6f64ca13ef9f502c01000
Aesistril <ae@aesistril.com>
2023-09-14 23:12:34 +0300

    add auto commit to link adding scripts

--- devscripts/mktemplink.sh
+++ devscripts/mktemplink.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cat template/tempredirect.html | sed "s|ae\-remote\-site|$1|g" > link/$2
+cat template/tempredirect.html | sed "s|ae\-remote\-site|$1|g" > link/$2.html
+git commit -a -m "add temporary link"
+git push
+echo Your link is live at share.aesistril.com/link/$2.html
