1b1f11b3eaa01f83cd0c1f32ce08fc4c252abc09
Aesistril <ae@aesistril.com>
2023-09-14 23:16:36 +0300

    fix git commands in devscripts

--- devscripts/mktemplink.sh
+++ devscripts/mktemplink.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cat template/tempredirect.html | sed "s|ae\-remote\-site|$1|g" > link/$2.html
-git commit -a -m "add temporary link"
+git add *
+git commit -m "add temporary link"
 git push
 echo Your link is live at share.aesistril.com/link/$2.html
