37403c62843303bb6aad9fb70b626ec480d637f6
Aesistril <ae@aesistril.com>
2023-09-26 13:08:46 +0300

    move temporary files to /temp/

--- devscripts/mktemplink.sh
+++ devscripts/mktemplink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cat template/tempredirect.html | sed "s|ae\-remote\-site|$1|g" > link/$2.html
+cat template/tempredirect.html | sed "s|ae\-remote\-site|$1|g" > temp/link/$2.html
 git add *
 git commit -m "add temporary link"
 git push
-echo Your link is live at share.aesistril.com/link/$2.html
+echo Your link is live at share.aesistril.com/temp/link/$2.html
